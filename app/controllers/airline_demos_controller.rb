8339b6626119b84d23068af42ccc5995b522d5eb
alazarlemma02 <alazar03lemma@gmail.com>
2023-10-14 22:32:21 +0300

    created a controller for the airline model

---
+++ app/controllers/airline_demos_controller.rb
@@ -0,0 +1,8 @@
+class AirlineDemosController < ApplicationController
+
+  def index
+    @airlines = AirlineDemo.includes(@airlines).all
+    render json: {success: true, data: @airlines}
+  end
+
+end
